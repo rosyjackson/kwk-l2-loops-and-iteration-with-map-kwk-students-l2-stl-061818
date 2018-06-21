adcd4036387b91a1ec21c44239f148d6d600d70b
rosyjackson <rosyjackson2004@gmail.com>
2018-06-21 18:36:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ return list.map(other)
 const students = ['Mary Cassatt', 'Joan Mitchell', 'Eva Hesse', 'Frida Kahlo', 'Taylor Swift', 'Agnes Martin'];
 
 function nameToAttributes(firstName,lastName) {
-
+return firstName.split(nameToAttributes)
 }
 
