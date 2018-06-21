8b610073bcee2703ef0f5615eeea215a4be6b032
rosyjackson <rosyjackson2004@gmail.com>
2018-06-21 18:31:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,4 @@ return list.map(other)
 
 const students = ['Mary Cassatt', 'Joan Mitchell', 'Eva Hesse', 'Frida Kahlo', 'Taylor Swift', 'Agnes Martin'];
 
-function nameToAttributes (names){[
-  { firstName: 'Mary',   lastName: 'Cassatt'     },
-  { firstName: 'Joan',   lastName: 'Mitchell'   },
-  { firstName: 'Eva',   lastName: 'Hesse'   },
-  { firstName: 'Frida', lastName: 'Kahlo'    },
-  { firstName: 'Taylor',   lastName: 'Swift' },
-  { firstName: 'Agnes',   lastName: 'Martin'     }
-]
- return names.split()
-}
+function nameToAttributes
