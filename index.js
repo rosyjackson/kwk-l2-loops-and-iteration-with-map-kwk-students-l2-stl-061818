20c9b866e4b6fdf59c55ae08e9043986190b8a9a
rosyjackson <rosyjackson2004@gmail.com>
2018-06-21 19:10:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file.
+
+function other (student){
+  return student.toLowerCase();
+}
+
+function lowerCaseStudentNames(list) {
+return list.map(other) 
+}
+
+let students = ['Amy Adams', 'Cate Blanchett', 'Emma Stone', 'Jennifer Lawrence', 'Natalie Portman'];
+
+function nameToAttributes(students){
+  return students.split();
+}
+
+function splitName(name) {
+  const studentFirst = name.split(" ")[0];
+const studentLast = name.split(" ")[1];
+ 
+ return {
+   firstName : studentFirst, lastName : studentLast
+ }; 
+}
+
+function nameToAttributes(list){
+  return list.map(splitName);
+}
+
+function attributesToPhrase(){
+  const = ''
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
