97b26e0b06cdca27d8cbedcab08284ca0d7c9138
rosyjackson <rosyjackson2004@gmail.com>
2018-06-21 18:18:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function lowerCaseStudentNames() {
   let studentNames = ['Adele', 'Beyoncé', 'Lady', 'Madonna', 'Rihanna', 'Taylor']
   return studentNames;
 }
+
+[
+  { firstName: 'Mary',   lastName: 'Cassatt'     },
+  { firstName: 'Joan',   lastName: 'Mitchell'   },
+  { firstName: 'Eva',   lastName: 'Hesse'   },
+  { firstName: 'Frida', lastName: 'Kahlo'    },
+  { firstName: 'Taylor',   lastName: 'Swift' },
+  { firstName: 'Agnes',   lastName: 'Martin'     }
+];
