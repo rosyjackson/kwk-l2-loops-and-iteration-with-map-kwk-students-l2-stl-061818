66645c735369ce86460a51cf09e3942c9a7915ed
rosyjackson <rosyjackson2004@gmail.com>
2018-06-21 18:31:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ return list.map(other)
 
 const students = ['Mary Cassatt', 'Joan Mitchell', 'Eva Hesse', 'Frida Kahlo', 'Taylor Swift', 'Agnes Martin'];
 
-function nameToAttributes (names){
+function nameToAttributes (names){[
+  { firstName: 'Mary',   lastName: 'Cassatt'     },
+  { firstName: 'Joan',   lastName: 'Mitchell'   },
+  { firstName: 'Eva',   lastName: 'Hesse'   },
+  { firstName: 'Frida', lastName: 'Kahlo'    },
+  { firstName: 'Taylor',   lastName: 'Swift' },
+  { firstName: 'Agnes',   lastName: 'Martin'     }
+]
  return names.split()
 }
